bd34cdd8f142d4573e4d073f1ad1400101418fff
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2020-07-31 17:58:54 -0500

    Added cli options

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from PIL import Image
+import sys
+
+if len(sys.argv) != 2:
+    print("[-] Usage: python main.py <image file>")
+    exit(1)
+
+imageFile = sys.argv[1]
 
 # Luminance Symbols 0.0 - 1.0
 lum_sym = ['.', '*', '^', '~', '!', '&', '$', '%', '#', '@']
